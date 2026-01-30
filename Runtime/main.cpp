b27c3d5e1f65d3a3538d5dd54ecc3c5e3f5a5561
Aurélien Rey <aurelien.rey@virtuosgames.com>
2026-01-30 17:11:28 +0100

    First commit

---
+++ Runtime/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <vulkan/vulkan.hpp>
+
+struct VulkanContext
+{
+	vk::Instance instance;
+};
+
+auto SetupInstanceAndDevices(VulkanContext& _context) -> int
+{
+	vk::ApplicationInfo appInfo("Audio", 0, "Audio", 0, VK_MAKE_API_VERSION(0, 1, 4, 0));
+	
+	std::vector<const char*> extensions { VK_KHR_SURFACE_EXTENSION_NAME, "VK_KHR_win32_surface" };
+	auto availableExtensions = vk::enumerateInstanceExtensionProperties();
+
+
+	for(const auto& extension : extensions)
+	{
+		bool found = false;
+
+		for(const auto& availableExtension : availableExtensions)
+		{
+			if(strcmp(extension, availableExtension.extensionName))
+			{
+				found = true;
+				break;
+			}
+		}
+
+		if(!found)
+		{
+			std::cerr << std::format("Could not validate extension {}", extension) << std::endl;
+			return -1;
+		}
+	}
+
+	vk::InstanceCreateInfo instanceInfo({}, &appInfo, 0, VK_NULL_HANDLE, static_cast<uint32_t>(extensions.size()), extensions.data());
+	_context.instance = vk::createInstance(instanceInfo);
+
+	return 0;
+}
+
+auto main() -> int
+{
+	std::cout << "Hello, World!" << std::endl;
+
+	VulkanContext context;
+	if(SetupInstanceAndDevices(context) != 0) return -1;
+
+	return 0;
+}
